971b9cb399e8a7f8be772efce94236916e5c231d
Aditya kumar <ak88564@gmail.com>
2024-11-07 15:29:23 +0530

    Added spring-boot-starter-security dependency

--- src/main/java/com/security/Controller/UserController.java
+++ src/main/java/com/security/Controller/UserController.java
@@ -1,7 +1,34 @@
-//package com.security.Controller;
-//
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class UserController {
-//}
+package com.security.Controller;
+
+import com.security.models.User;
+import com.security.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userservice;
+
+    @GetMapping("/")
+    public List<User> getAllUsers(){
+        return userservice.getAllUsers();
+    }
+
+    // return single user
+    @GetMapping("/{username}")
+    public User getUser(@PathVariable("username") String username){
+        return userservice.getUser(username);
+    }
+
+    @PostMapping("/")
+    public User add(@RequestBody User user){ // requestbody deserializes the json into java object
+        return userservice.addUser(user);
+    }
+
+
+}
--- src/main/java/com/security/services/UserService.java
+++ src/main/java/com/security/services/UserService.java
@@ -1,27 +1,33 @@
-//package com.security.services;
-//
-//
-//import com.security.models.User;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Service
-//public class UserService {
-//
-//    List<User> list = new ArrayList<User>();
-//
-//    public UserService() {
-//        list.add(new User("abc", "abc", "abc@gmail.com"));
-//        list.add(new User("xyz", "abcxyz", "xyz@gmail.com"));
-//    }
-//
-//    public List<User> getAllUsers(){
-//        return this.list;
-//    }
-//
-////    public User getUser(final String username){
-////        return this.list.stream().filter((user) -> user.getUsername().equals(username)).findAny().orElse(null);
-////    }
-//}
+package com.security.services;
+
+
+import com.security.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    List<User> list = new ArrayList<>();
+
+    public UserService() {
+        list.add(new User("abc", "abc", "abc@gmail.com"));
+        list.add(new User("xyz", "abcxyz", "xyz@gmail.com"));
+    }
+
+    public List<User> getAllUsers(){
+        return this.list;
+    }
+
+    public User getUser(final String username){
+        return this.list.stream().filter((user) -> user.getUsername().equals(username)).findAny().orElse(null);
+    }
+
+    // add new user
+    public User addUser(User user){
+        this.list.add(user);
+        return user;
+    }
+}
